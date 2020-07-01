e94756f6a801c1581219eb8f8c154f16f9f0bf40
Bruno G Alcantara <inovox@gmail.com>
2020-06-30 22:19:30 -0300

    first commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    public function index(Request $request): Response {
+        $nome = $request->query->get('nome');
+        $sobrenome = $request->query->get('sobrenome');
+        /* para testar: http://localhost:8080/?nome=lucas&sobrenome=alcantara */
+
+        return $this->render('base.html.twig',[
+            'nome' => $nome,
+            'sobrenome' => $sobrenome
+        ]);
+    }
+}
