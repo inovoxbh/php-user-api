dd4fccce1b531f3a8c41eef2d6d4d960a2401d8b
Bruno G Alcantara <inovox@gmail.com>
2020-07-02 21:12:28 -0300

    entity user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,9 +1,6 @@
 <?php
-
 namespace App\Entity;
-
 use Doctrine\ORM\Mapping as ORM;
-
 /**
  * @ORM\Entity()
  */
@@ -11,48 +8,50 @@ class User
 {
     /**
      * @ORM\Id()
+     * @ORM\Column()
      * @ORM\GeneratedValue()
      */
     private ?int $id = null;
-
     /**
      * @ORM\Column()
      */
     private ?string $name = null;
-
     /**
      * @ORM\Column()
      */
     private ?string $email = null;
-
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private ?\DateTime $createdDate = null;
     public function __construct(string $name, string $email)
     {
         $this->name = $name;
         $this->email = $email;
+        $this->createdDate = new \DateTime();
     }
-
     public function getId(): ?int
     {
         return $this->id;
     }
-
     public function getName(): ?string
     {
         return $this->name;
     }
-
     public function setName(string $name): void
     {
         $this->name = $name;
     }
-
     public function getEmail(): ?string
     {
         return $this->email;
     }
-
     public function setEmail(string $email): void
     {
         $this->email = $email;
+    }
+    public function getCreatedDate(): ?\DateTime
+    {
+        return $this->createdDate;
     }
 }
